32fd3b50be7437d759302fae9b3c75320c4ffd8c
belastars <bela_baroni@hotmail.com>
2019-09-30 19:02:36 -0300

    Aula 8

---
+++ script.js
@@ -0,0 +1,117 @@
+// olá mundo;
+
+//alert("Olá Mundo");   1. Faça uma mensagem de "Olá Mundo!"
+
+//2. Faça uma saída de soma de 2 números.
+
+// Soma po Menos 
+//var num1 = prompt("Digite um numero:");
+//var num2 = prompt("Digite um numero:");
+//var result  = parseInt(num1) + parseInt(num2);
+//alert(result);
+
+
+
+//3. Utilizando variáveis, solicite o ano de nascimento do usuário, calcule sua idade e mostre em um alert.
+
+//var anoNasc = prompt("Digite o Ano do seu nascimento:");
+//var anoAtual = prompt("Digite o Ano Atual:");
+//var resultado  = parseInt(anoNasc)-parseInt(anoAtual);
+//alert("O Ano do seu nascimento é: "+resultado);
+
+
+// OUTRO EXEMPLO
+// var anoNasc = prompt("Digite seu ano de nascimento:")
+// var idade = 2019 + anoNasc;
+//alert(idade);
+
+
+
+
+//4. Receba do usuários 3 números e calcule a média entre eles.
+
+//var med1 = prompt("Digite o 1 numero:");
+//var med2 = prompt("Digite o 2 numero:");
+//var med3 = prompt("Digite o 3 numero:");
+//var med  = (parseInt(med1) + parseInt(med2) + parseInt(med3)) / 3;
+//alert("O resultado da media é: "+ med);
+
+// var medis = (1 + 2 + 3) / 3 =;
+
+
+
+//matematica
+
+//var x =10;
+//var y =15;
+//x += y;
+//console.log(x);
+
+
+//console.log (1 !== "1");   booleano
+
+// console.log(!%2); inteiro
+
+
+
+//var x = 1;
+//console.log(x);
+
+//x++;
+//console.log(x);
+
+//++x;     ESTA FORMA PODE DAR BO
+//console.log(x);
+
+
+/*var x = 5 ;
+console.log(x);
+
+var result = 2+ (x++);
+console.log(6);
+
+var result = (x++)+2;   ecrementou priemrio e depois colocou mas 2
+console.log(8);
+*/
+//1. Crie uma função que receba 1 parâmetro e calcule 5% de desconto, retornando o valor do desconto;
+
+
+//function calcularDesconto(valor){
+//  let desconto = valor * 0.05;
+//  console.log(desconto);
+//  return desconto;  }
+//let resultado = calcularDesconto(67);
+
+//2. Crie uma função que receba 2 parâmetros e exiba o resultado e o resto da divisão entre eles;
+
+//function calcularDivisao(valor1,valor2){
+//  let divisao = valor1 / valor2;
+//  let  resto = valor1%valor2;
+//  console.log("O resultado da divisao é: " + divisao + "e o resto é:"+ resto); 
+//}
+//calcularDivisao(6,7)
+
+
+
+//3. Crie uma função que recebe 2 parâmetros (altura e peso) e retorna o IMC;
+
+//function calcularImc(peso,altura){
+   //let imc = peso/(altura **2);
+   //console.log("O resultado do IMC é: " + imc); 
+//}
+
+//calcularImc(45,1.55)
+
+
+//4. Crie uma função que recebe 3 parâmetros, e utilizando a fórmula de Báskara, mostre o resultado de x' e x".
+
+
+//6. Crie uma função que calcule quantos dias o usuário viveu, baseado na idade;
+
+//let id  = Number(prompt("Qual é sua idade:"));
+
+//function calculaViveu(a){  
+//  let calculo =  365*id; 
+//  console.log("A quantidade de dias que você viveu foi:" + calculo);
+//}
+//calculaViveu();
